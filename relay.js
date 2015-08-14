e517eeac08f9114ecd41796eed5ba4c6d7b62fcb
queue <queue@hakase.org>
2015-08-13 18:35:38 -0600

    Add more error handling
    Not really sure how much is necessary, but
    crashing the server is annoying.

--- relay.js
+++ relay.js
@@ -6,7 +6,7 @@ http = require('http');
 https = require('https');
 METAINT = 16000;
 http.createServer(function(req, res){
-  var get;
+  var err, get;
   if (req.method !== 'GET') {
     res.statusCode = 405;
     res.end();
@@ -17,7 +17,16 @@ http.createServer(function(req, res){
     res.end();
     return;
   }
-  get = https.get({
+  err = function(e){
+    console.error(e);
+    try {
+      res.end(500);
+    } catch (e$) {
+      e = e$;
+      console.error(e);
+    }
+  };
+  return get = https.get({
     host: 'stream.r-a-d.io',
     path: '/main.mp3',
     headers: {
@@ -25,18 +34,23 @@ http.createServer(function(req, res){
     }
   }, function(hres){
     var i, w, mpg321, oggenc;
+    hres.on('error', err);
     i = new icy.Reader(hres.headers['icy-metaint']);
+    i.on('error', err);
     hres.pipe(i);
     w = new icy.Writer(METAINT);
+    w.on('error', err);
     i.on('metadata', function(m){
       console.error(icy.parse(m));
       w.queue(icy.parse(m));
     });
     mpg321 = cp.spawn('mpg321', ['-q', '-s', '-']);
     mpg321.stderr.pipe(process.stderr);
+    mpg321.on('error', err);
     i.pipe(mpg321.stdin);
     oggenc = cp.spawn('oggenc', ['-Q', '-r', '-b', '48', '-']);
     oggenc.stderr.pipe(process.stderr);
+    oggenc.on('error', err);
     mpg321.stdout.pipe(oggenc.stdin);
     res.writeHead(200, {
       'Content-Type': 'audio/ogg',
@@ -46,16 +60,18 @@ http.createServer(function(req, res){
     oggenc.stdout.pipe(w);
     w.pipe(res);
     res.on('close', function(){
-      oggenc.kill();
-      mpg321.kill();
-      get.abort();
+      if (oggenc != null) {
+        oggenc.kill();
+      }
+      if (mpg321 != null) {
+        mpg321.kill();
+      }
+      if (get != null) {
+        get.abort();
+      }
       console.error('closed!');
     });
-  });
-  return get.on('error', function(e){
-    console.error(e);
-    res.end(500);
-  });
+  }, get.on('error', err));
 }).listen(process.argv[2], function(){
   console.error("listening on " + process.argv[2]);
 });
--- relay.ls
+++ relay.ls
@@ -20,14 +20,23 @@ http.create-server (req, res) ->
     res.status-code = 404
     res.end!
     return
+  err = (e) !->
+    console.error e
+    try
+      res.end 500
+    catch
+      console.error e
   get = https.get do
     host: \stream.r-a-d.io
     path: \/main.mp3
     headers: 'Icy-MetaData' : 1
     (hres) !->
+      hres.on \error err
       i = new icy.Reader hres.headers[\icy-metaint]
+      i.on \error err
       hres.pipe i
       w = new icy.Writer METAINT
+      w.on \error err
       i.on \metadata (m) !->
         console.error icy.parse m
         w.queue icy.parse m
@@ -44,10 +53,12 @@ http.create-server (req, res) ->
 
       mpg321 = cp.spawn \mpg321 [\-q \-s \-]
       mpg321.stderr.pipe process.stderr
+      mpg321.on \error err
       i.pipe mpg321.stdin
 
       oggenc = cp.spawn \oggenc [\-Q \-r \-b \48 \-]
       oggenc.stderr.pipe process.stderr
+      oggenc.on \error err
       mpg321.stdout.pipe oggenc.stdin
 
       res.write-head 200,
@@ -60,14 +71,12 @@ http.create-server (req, res) ->
       w.pipe res
 
       res.on \close !->
-        oggenc.kill!
-        mpg321.kill!
-        get.abort!
+        oggenc?kill!
+        mpg321?kill!
+        get?abort!
         console.error \closed!
+    get.on \error err
 
-  get.on \error (e) !->
-    console.error e
-    res.end 500
 .listen process.argv[2], !->
   console.error "listening on #{process.argv[2]}"
 
